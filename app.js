a38892574fc915f3e26743380d1c5d87c8f5c735
Megan Lee <megselayne@gmail.com>
2020-07-18 20:31:18 -0400

    user can select starter pokemon

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+//variable declarations
 const attackButton = document.querySelector('.battle-click');
 const battleVis = document.querySelector('.attack-vis');
+const pokeStarters = document.querySelectorAll('.poke');
+
+const userPlayer={
+    starter: null,
+}
 const actionStart = function(){
     console.log('heard the click');
     battleVis.style.display = 'inline-block';
     battleVis.style.animation = 'attack-s 4s';
 };
 
+const selectStarter = (event)=>{
+    console.log('pokemon selected!')
+    userPlayer.starter = event.target;
+}
+//event listeners
+pokeStarters.forEach(element=>{
+    element.addEventListener('click',selectStarter);
+})
 attackButton.addEventListener('click', actionStart);
