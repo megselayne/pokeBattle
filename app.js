64f9f500dd0a4866574ec48f7918ac2c3ec52b14
Megan Lee <megselayne@gmail.com>
2020-07-19 09:45:58 -0400

    got repeated animation to work!

--- app.js
+++ app.js
@@ -2,19 +2,35 @@
 const attackButton = document.querySelector('.battle-click');
 const battleVis = document.querySelector('.attack-vis');
 const pokeStarters = document.querySelectorAll('.poke');
+const titles = document.querySelector('h2.title-text');
+const attackVis = document.querySelector('.attack-vis');
 
 const userPlayer={
     starter: null,
+    selection(){
+        titles.innerHTML = `You chose ${this.starter.getAttribute('id')}`;
+    },
 }
-const actionStart = function(){
+const actionStart =()=>{
     console.log('heard the click');
-    battleVis.style.display = 'inline-block';
-    battleVis.style.animation = 'attack-s 4s';
+    if(! attackVis.classList.contains('animation')){
+        console.log('knows it does not contain');
+        attackVis.classList.add('animation');
+    }
+    else{
+        attackVis.classList.remove('animation');
+        //add back after delay
+        setTimeout(function(){
+            attackVis.classList.add('animation');
+        },1000 *3);
+    }
+    
 };
 
 const selectStarter = (event)=>{
     console.log('pokemon selected!')
     userPlayer.starter = event.target;
+    userPlayer.selection();
 }
 //event listeners
 pokeStarters.forEach(element=>{
